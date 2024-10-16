8e5d39d5f9959a823e64ac6bcfd5f3ba26cdc01c
zerodeok <dudejr3165@naver.com>
2024-10-16 17:11:48 +0900

    머지 충돌 다시 해결

--- src/main/java/com/sparta/springtrello/domain/user/dto/response/GetProfileResponseDto.java
+++ src/main/java/com/sparta/springtrello/domain/user/dto/response/GetProfileResponseDto.java
@@ -1,6 +1,6 @@
 package com.sparta.springtrello.domain.user.dto.response;
 
-import com.sparta.springtrello.domain.user.UserRole.UserRole;
+import com.sparta.springtrello.domain.user.authority.Authority;
 import com.sparta.springtrello.entity.User;
 import lombok.Getter;
 
@@ -8,11 +8,11 @@ import lombok.Getter;
 public class GetProfileResponseDto {
     private final String email;
     private final String nickName;
-    private final UserRole userRole;
+    private final Authority authority;
 
     public GetProfileResponseDto(User user) {
         this.email = user.getEmail();
         this.nickName = user.getNickName();
-        this.userRole = user.getUserRole();
+        this.authority = user.getAuthority();
     }
 }
